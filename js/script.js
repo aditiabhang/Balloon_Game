0e4301333a852ec2c5789787ccae8bf4d7c0c514
Aditi Abhang <aditi.abhang91@gmail.com>
2021-02-15 20:10:32 -0600

    Update style.js file

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ let colors = ['red', 'yellow', 'blue', 'green', 'violet'];
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 let body = document.body;
+let scores = document.querySelectorAll('.score');
+let num = 0;
+let total = 10;
+let currentBalloon = 0;
+let gameOver = false;
+let totalShadow = document.querySelector('.total-shadow');
 
-// Funtion that creates a balloon
+// Function that creates a balloon
 function createBalloon () {
     let div = document.createElement('div');
     let rand = Math.floor(Math.random() * colors.length);
@@ -11,6 +17,8 @@ function createBalloon () {
     
     rand = Math.floor(Math.random() * (windowWidth - 100));
     div.style.left = rand + 'px';
+    div.dataset.number = currentBalloon;
+    currentBalloon++;
     
     // adding the balloon on the webpage
     body.appendChild(div);
@@ -23,9 +31,9 @@ function animateBalloon(elem) {
     let interval = setInterval(frame, 10);
 
     function frame() {
-        if(pos >= windowHeight + 200) {
+        if(pos >= windowHeight + 200 && document.querySelector('[data-number="'+elem.dataset.number+'"]') != null) {
             clearInterval(interval);
-            deleteBalloon(elem);
+            gameOver = true;
         } else {
             pos++;
             elem.style.top = windowHeight - pos + 'px';
@@ -36,6 +44,30 @@ function animateBalloon(elem) {
 // Removing the balloon when it reached the window height
 function deleteBalloon(elem) {
     elem.remove();
+    num++;
+    updateScore();
+}
+
+function updateScore() {
+    for(let i=0; i<scores.length; i++) {
+        scores[i].textContent = num;
+    }
+}
+
+function startGame() {
+    let loop = setInterval(function() {
+        if(!gameOver && num !== total) {
+            createBalloon();
+        } else if(num !== total) {
+            clearInterval(loop);
+            totalShadow.style.display = 'flex';
+            totalShadow.querySelector('.lose').style.display = 'block';
+        } else if(num == total) {
+            clearInterval(loop);
+            totalShadow.style.display = 'flex';
+            totalShadow.querySelector('.win').style.display = 'block';
+        }
+    }, 800);
 }
 // ----Event Delegation -----
 // Attaching an event listener on a web page not only 
@@ -46,3 +78,5 @@ document.addEventListener('click', function(event) {
         deleteBalloon(event.target);
     }
 });
+
+startGame();
