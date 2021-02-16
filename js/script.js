89d5f87e24574c436562c774225094d8e03745d9
Aditi Abhang <aditi.abhang91@gmail.com>
2021-02-16 11:20:18 -0600

    Game is ready to play.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ let windowHeight = window.innerHeight;
 let body = document.body;
 let scores = document.querySelectorAll('.score');
 let num = 0;
-let total = 10;
+let total = 100;
 let currentBalloon = 0;
 let gameOver = false;
 let totalShadow = document.querySelector('.total-shadow');
+let startBtn = document.querySelector('.start-game-button');
 
 // Function that creates a balloon
 function createBalloon () {
@@ -28,7 +29,8 @@ function createBalloon () {
 // Making the balloon float to the top
 function animateBalloon(elem) {
     let pos = 0;
-    let interval = setInterval(frame, 10);
+    let random = Math.floor(Math.random() * 6 - 3);
+    let interval = setInterval(frame, 12 - Math.floor(num / 10) + random);
 
     function frame() {
         if(pos >= windowHeight + 200 && document.querySelector('[data-number="'+elem.dataset.number+'"]') != null) {
@@ -46,6 +48,7 @@ function deleteBalloon(elem) {
     elem.remove();
     num++;
     updateScore();
+    playBalloonSound();
 }
 
 function updateScore() {
@@ -55,7 +58,10 @@ function updateScore() {
 }
 
 function startGame() {
+    restartGame();
+    let timeout = 0;
     let loop = setInterval(function() {
+        timeout = Math.floor(Math.random() * 600 - 100);
         if(!gameOver && num !== total) {
             createBalloon();
         } else if(num !== total) {
@@ -67,8 +73,19 @@ function startGame() {
             totalShadow.style.display = 'flex';
             totalShadow.querySelector('.win').style.display = 'block';
         }
-    }, 800);
+    }, 800 + timeout);
 }
+
+function restartGame() {
+    let forRemoving = document.querySelectorAll('.balloon');
+    for(let i=0; i<forRemoving.length; i++) {
+        forRemoving[i].remove();
+    }
+    gameOver = false;
+    num = 0;
+    updateScore();
+}
+
 // ----Event Delegation -----
 // Attaching an event listener on a web page not only 
 // to an existing element but also the elements that are not yet created.
@@ -79,4 +96,28 @@ document.addEventListener('click', function(event) {
     }
 });
 
-startGame();
+// Adding sound when balloons get popped 
+function playBalloonSound() {
+    let audio = document.createElement('audio');
+    audio.src = 'sounds/pop.mp3';
+    audio.play();
+}
+
+// Adding button functionalities 
+document.querySelector('.restart').addEventListener('click', function() {
+    totalShadow.style.display = 'none';
+    totalShadow.querySelector('.win').style.display = 'none';
+    totalShadow.querySelector('.lose').style.display = 'none';
+    startGame();
+});
+
+document.querySelector('.cancel').addEventListener('click', function() {
+    totalShadow.style.display = 'none';
+});
+
+// Adding background music
+startBtn.addEventListener('click', function() {
+    startGame();
+    document.querySelector('.bg-music').play();
+    document.querySelector('.start-game-window').style.display = 'none';  
+});
